5b2dbecfa96d4072adbef98aed445721c9acd823
Moxie <rain49048@gmail.com>
2019-10-06 13:18:19 -0400

    Add project files.

---
+++ Controllers/AlarmClockController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoxieMillion.Controllers
+{
+    public class AlarmClockController : Controller
+    {
+
+        public IActionResult Clock()
+        {
+            return View();
+        }
+
+        public IActionResult Destination()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MoxieMillion.Models;
+
+namespace MoxieMillion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+       
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Destination()
+        {
+            return View();
+        }
+
+        public IActionResult Clock()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
